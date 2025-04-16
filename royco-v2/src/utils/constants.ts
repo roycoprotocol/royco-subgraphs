f08a127a9ccbb7f2322c2d4cb063521852facf1a
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-04-15 18:17:26 -0700

    revised multiplier markewt hub handler logic

--- royco-v2/src/utils/constants.ts
+++ royco-v2/src/utils/constants.ts
@@ -11,6 +11,11 @@ export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export const BIG_INT_ZERO = new BigInt(0);
 
+export enum OFFER_TYPE {
+    AP_OFFER,
+    IP_OFFER
+}
+
 // Multipliers are denominated in BPS, so 100 = 1x multiplier
 export const DEFAULT_MULTIPLIER = new BigInt(100);
 
