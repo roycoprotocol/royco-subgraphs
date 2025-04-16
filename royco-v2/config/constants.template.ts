41cdda6dacb7e666460cfbbf8ea3ac7902b0c8c6
Kevin O'Connell <kevinoconnell42@gmail.com>
2025-04-16 09:57:34 -0700

    add v2 templates

--- royco-v2/config/constants.template.ts
+++ royco-v2/config/constants.template.ts
@@ -1,7 +1,7 @@
 // prettier-ignore
 import { BigInt } from "@graphprotocol/graph-ts";
 
-export const CHAIN_ID = BigInt.fromU64(11155111); // Will be replaced during deployment
+export const CHAIN_ID = BigInt.fromU64({{chainId}}); // Will be replaced during deployment
 
 export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ZERO_BD = "0";
