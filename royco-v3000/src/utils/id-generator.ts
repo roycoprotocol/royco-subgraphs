7473e8161de757e34b5dfd4ce9fdfedd0264e01c
Kevin O'Connell <kevinoconnell42@gmail.com>
2025-06-23 15:30:54 -0700

    lowercase

--- royco-v3000/src/utils/id-generator.ts
+++ royco-v3000/src/utils/id-generator.ts
@@ -4,50 +4,55 @@ import { CHAIN_ID } from "../constants";
 /**
  * Generate a unique ID for events based on transaction hash and log index
  */
-export function generateEventId(transactionHash: Bytes, logIndex: BigInt): string {
+export function generateEventId(
+  transactionHash: Bytes,
+  logIndex: BigInt
+): string {
   return CHAIN_ID.toString()
     .concat("_")
-    .concat(transactionHash.toHexString())
+    .concat(transactionHash.toHexString().toLowerCase())
     .concat("_")
-    .concat(logIndex.toString());
+    .concat(logIndex.toString().toLowerCase());
 }
 
 /**
  * Generate a unique ID for Safe entities based on chain ID and safe address
  */
 export function generateRawSafeId(safeAddress: string): string {
-  return CHAIN_ID.toString()
-    .concat("_")
-    .concat(safeAddress);
+  return CHAIN_ID.toString().concat("_").concat(safeAddress.toLowerCase());
 }
 
 /**
  * Generate a unique ID for Safe-Account mapping based on safe address and account address
  */
-export function generateRawSafeMapId(safeAddress: string, accountAddress: string): string {
+export function generateRawSafeMapId(
+  safeAddress: string,
+  accountAddress: string
+): string {
   return CHAIN_ID.toString()
     .concat("_")
-    .concat(safeAddress)
+    .concat(safeAddress.toLowerCase())
     .concat("_")
-    .concat(accountAddress);
+    .concat(accountAddress.toLowerCase());
 }
 
 /**
  * Generate a unique ID for tokenized positions based on safe address and token address
  */
-export function generateRawSafeTokenizedPositionId(safeAddress: string, tokenAddress: string): string {
+export function generateRawSafeTokenizedPositionId(
+  safeAddress: string,
+  tokenAddress: string
+): string {
   return CHAIN_ID.toString()
     .concat("_")
-    .concat(safeAddress)
+    .concat(safeAddress.toLowerCase())
     .concat("_")
-    .concat(tokenAddress);
+    .concat(tokenAddress.toLowerCase());
 }
 
 /**
  * Generate a token ID (used for cross-reference)
  */
 export function generateTokenId(tokenAddress: string): string {
-  return CHAIN_ID.toString()
-    .concat("-")
-    .concat(tokenAddress);
+  return CHAIN_ID.toString().concat("-").concat(tokenAddress.toLowerCase());
 }
