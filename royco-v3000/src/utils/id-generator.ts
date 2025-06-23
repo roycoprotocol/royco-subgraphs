bd7edfbb175b470f091c2a45dc7adaef93e26b02
Kevin O'Connell <kevinoconnell42@gmail.com>
2025-06-23 14:36:46 -0700

    init

---
+++ royco-v3000/src/utils/id-generator.ts
@@ -0,0 +1,53 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { CHAIN_ID } from "../constants";
+
+/**
+ * Generate a unique ID for events based on transaction hash and log index
+ */
+export function generateEventId(transactionHash: Bytes, logIndex: BigInt): string {
+  return CHAIN_ID.toString()
+    .concat("_")
+    .concat(transactionHash.toHexString())
+    .concat("_")
+    .concat(logIndex.toString());
+}
+
+/**
+ * Generate a unique ID for Safe entities based on chain ID and safe address
+ */
+export function generateRawSafeId(safeAddress: string): string {
+  return CHAIN_ID.toString()
+    .concat("_")
+    .concat(safeAddress);
+}
+
+/**
+ * Generate a unique ID for Safe-Account mapping based on safe address and account address
+ */
+export function generateRawSafeMapId(safeAddress: string, accountAddress: string): string {
+  return CHAIN_ID.toString()
+    .concat("_")
+    .concat(safeAddress)
+    .concat("_")
+    .concat(accountAddress);
+}
+
+/**
+ * Generate a unique ID for tokenized positions based on safe address and token address
+ */
+export function generateRawSafeTokenizedPositionId(safeAddress: string, tokenAddress: string): string {
+  return CHAIN_ID.toString()
+    .concat("_")
+    .concat(safeAddress)
+    .concat("_")
+    .concat(tokenAddress);
+}
+
+/**
+ * Generate a token ID (used for cross-reference)
+ */
+export function generateTokenId(tokenAddress: string): string {
+  return CHAIN_ID.toString()
+    .concat("-")
+    .concat(tokenAddress);
+}
