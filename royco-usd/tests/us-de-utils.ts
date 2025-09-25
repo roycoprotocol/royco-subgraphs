a7e756b90050727532065e0383dcf888344834e7
githubotoro <yupuday.git@gmail.com>
2025-09-25 10:33:12 -0700

    Initialize subgraph

---
+++ royco-usd/tests/us-de-utils.ts
@@ -0,0 +1,124 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  EIP712DomainChanged,
+  MinterUpdated,
+  OwnershipTransferStarted,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/USDe/USDe"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createEIP712DomainChangedEvent(): EIP712DomainChanged {
+  let eip712DomainChangedEvent = changetype<EIP712DomainChanged>(newMockEvent())
+
+  eip712DomainChangedEvent.parameters = new Array()
+
+  return eip712DomainChangedEvent
+}
+
+export function createMinterUpdatedEvent(
+  newMinter: Address,
+  oldMinter: Address
+): MinterUpdated {
+  let minterUpdatedEvent = changetype<MinterUpdated>(newMockEvent())
+
+  minterUpdatedEvent.parameters = new Array()
+
+  minterUpdatedEvent.parameters.push(
+    new ethereum.EventParam("newMinter", ethereum.Value.fromAddress(newMinter))
+  )
+  minterUpdatedEvent.parameters.push(
+    new ethereum.EventParam("oldMinter", ethereum.Value.fromAddress(oldMinter))
+  )
+
+  return minterUpdatedEvent
+}
+
+export function createOwnershipTransferStartedEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferStarted {
+  let ownershipTransferStartedEvent =
+    changetype<OwnershipTransferStarted>(newMockEvent())
+
+  ownershipTransferStartedEvent.parameters = new Array()
+
+  ownershipTransferStartedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferStartedEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferStartedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent =
+    changetype<OwnershipTransferred>(newMockEvent())
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
