11e9c0f8d318a6c87e4fa41f0acf8544780b2290
githubotoro <yupuday.git@gmail.com>
2026-01-19 16:24:58 -0800

    feat(config): templates updated

--- royco-rwa/src/constants/static.ts
+++ royco-rwa/src/constants/static.ts
@@ -2,5 +2,5 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export const CHAIN_ID = BigInt.fromU64(1);
-export const VAULT_MAJOR_TYPE = "earn";
-export const VAULT_MINOR_TYPE = "concrete";
+export const VAULT_MAJOR_TYPE = "market";
+export const VAULT_MINOR_TYPE = "unknown";
