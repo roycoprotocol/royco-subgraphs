09c9a9da463a7f836ff192ae45162873e5f1311b
githubotoro <yupuday.git@gmail.com>
2026-07-02 08:54:18 -0700

    feat(subgraphs): update

--- royco-rwa/src/constants/static.ts
+++ royco-rwa/src/constants/static.ts
@@ -1,11 +1,6 @@
 // prettier-ignore
 import { BigInt } from "@graphprotocol/graph-ts";
 
-export const CHAIN_ID = BigInt.fromU64(42161);
-
-export function getEntryPointVersion(address: string): BigInt {
-  let normalized = address.toLowerCase();
-  if (normalized == "0xe72b67389e6c8919961b4eb2b9563b9359ba30b6".toLowerCase()) return BigInt.fromU64(1);
-  if (normalized == "0x63da1229be88fb4d20210147954a1a3e05f2581b".toLowerCase()) return BigInt.fromU64(2);
-  return BigInt.zero();
-}
+export const CHAIN_ID = BigInt.fromU64(1);
+export const VAULT_MAJOR_TYPE = "marketv2";
+export const VAULT_MINOR_TYPE = "unknown";
